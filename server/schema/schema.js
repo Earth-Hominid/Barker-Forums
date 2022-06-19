fae284ad181a94e5fbedce427bd5f631f7ea45d2
C <earthhominid@gmail.com>
2022-06-19 10:51:45 -0300

    graphql schema's updated, logic working without errors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,9 @@
+// Mongoose Models
 const Subforum = require('../models/Subforum');
 const User = require('../models/User');
 const Post = require('../models/Post');
 const Comment = require('../models/Comment');
+// const UserType = require('./userSchema');
 
 const {
   GraphQLObjectType,
@@ -13,7 +15,7 @@ const {
   GraphQLEnumType,
 } = require('graphql');
 
-// Subforum Type
+//Subforum Type
 const SubforumType = new GraphQLObjectType({
   name: 'Subforum',
   fields: () => ({
@@ -32,11 +34,31 @@ const SubforumType = new GraphQLObjectType({
   }),
 });
 
+// User Type
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLID },
+    username: { type: GraphQLString },
+    email: { type: GraphQLString },
+    friends: { type: GraphQLList(UserType) },
+    createdSubforums: { type: GraphQLList(SubforumType) },
+    memberSubforums: { type: GraphQLList(SubforumType) },
+    createdPosts: { type: GraphQLList(PostType) },
+    savedPosts: { type: GraphQLList(PostType) },
+    createdComments: { type: GraphQLList(CommentType) },
+    savedComments: { type: GraphQLList(CommentType) },
+    postVoteCount: { type: GraphQLInt },
+    commentVoteCount: { type: GraphQLInt },
+  }),
+});
+
 // Post Type
 const PostType = new GraphQLObjectType({
   name: 'Post',
   fields: () => ({
     id: { type: GraphQLID },
+    title: { type: GraphQLString },
     content: { type: GraphQLString },
     votes: { type: GraphQLEnumType },
     comments: { type: GraphQLList(CommentType) },
@@ -60,7 +82,6 @@ const CommentType = new GraphQLObjectType({
   name: 'Comment',
   fields: () => ({
     id: { type: GraphQLID },
-    title: { type: GraphQLString },
     content: { type: GraphQLString },
     votes: { type: GraphQLEnumType },
     subforum: {
@@ -84,72 +105,62 @@ const CommentType = new GraphQLObjectType({
   }),
 });
 
-// Client Type
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: () => ({
-    id: { type: GraphQLID },
-    username: { type: GraphQLString },
-    email: { type: GraphQLString },
-  }),
-});
-
-const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    subforums: {
-      type: new GraphQLList(SubforumType),
-      resolve(parent, args) {
-        return Subforum.find();
-      },
-    },
-    subforum: {
-      type: SubforumType,
-      ars: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return Subforum.findById(args.id);
-      },
-    },
-    users: {
-      type: new GraphQLList(UserType),
-      resolve(parent, args) {
-        return User.find();
-      },
-    },
-    user: {
-      type: UserType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return User.findById(args.id);
-      },
-    },
-  },
-});
+// const RootQuery = new GraphQLObjectType({
+//   name: 'RootQueryType',
+//   fields: {
+//     subforums: {
+//       type: new GraphQLList(SubforumType),
+//       resolve(parent, args) {
+//         return Subforum.find();
+//       },
+//     },
+//     subforum: {
+//       type: SubforumType,
+//       ars: { id: { type: GraphQLID } },
+//       resolve(parent, args) {
+//         return Subforum.findById(args.id);
+//       },
+//     },
+//     users: {
+//       type: new GraphQLList(UserType),
+//       resolve(parent, args) {
+//         return User.find();
+//       },
+//     },
+//     user: {
+//       type: UserType,
+//       args: { id: { type: GraphQLID } },
+//       resolve(parent, args) {
+//         return User.findById(args.id);
+//       },
+//     },
+//   },
+// });
 
 // Mutations
-const mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: {
-    // Add user
-    addUser: {
-      type: UserType,
-      args: {
-        username: { type: GraphQLNonNull(GraphQLString) },
-        email: { type: GraphQLNonNull(GraphQLString) },
-      },
-      resolve(parent, args) {
-        const client = new Client({
-          username: args.username,
-          email: args.email,
-        });
+// const mutation = new GraphQLObjectType({
+//   name: 'Mutation',
+//   fields: {
+//     // Add user
+//     addUser: {
+//       type: UserType,
+//       args: {
+//         username: { type: GraphQLNonNull(GraphQLString) },
+//         email: { type: GraphQLNonNull(GraphQLString) },
+//       },
+//       resolve(parent, args) {
+//         const client = new Client({
+//           username: args.username,
+//           email: args.email,
+//         });
 
-        return client.save();
-      },
-    },
-  },
-});
+//         return client.save();
+//       },
+//     },
+//   },
+// });
 
-module.exports = new GraphQLSchema({
-  query: RootQuery,
-  mutation,
-});
+// module.exports = new GraphQLSchema({
+//   query: RootQuery,
+//   mutation,
+// });
