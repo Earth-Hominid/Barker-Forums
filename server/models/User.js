79ae6981a95c84a72b9a0276b7a7d35972e8a1fd
C <earthhominid@gmail.com>
2022-06-19 10:54:04 -0300

    added remaining fields to user mongoose model

--- server/models/User.js
+++ server/models/User.js
@@ -7,6 +7,15 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+  friends: { type: Array },
+  createdSubforums: { type: Array },
+  memberSubforums: { type: Array },
+  createdPosts: { type: Array },
+  savedPosts: { type: Array },
+  createdComments: { type: Array },
+  savedCommetns: { type: Array },
+  postVoteCount: { type: Number },
+  commentVoteCount: { type: Number },
 });
 
 module.exports = mongoose.model('User', UserSchema);
