b57c1ca36d28ac5156393525819cb84392dcffb4
C <earthhominid@gmail.com>
2022-06-21 18:52:58 -0300

    refactored user query logic, added spinner component

--- client/src/components/users/Users.js
+++ client/src/components/users/Users.js
@@ -1,22 +1,15 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_USERS = gql`
-  query getUsers {
-    users {
-      id
-      username
-      email
-    }
-  }
-`;
+import { useQuery } from '@apollo/client';
+import { GET_USERS } from '../../queries/UserQueries';
+import Spinner from '../../components/spinner/Spinner';
+import UserTable from '../tables/UserTable';
 
 const Users = () => {
   const { loading, error, data } = useQuery(GET_USERS);
 
-  if (loading) return <>Loading...</>;
+  if (loading) return <Spinner />;
   if (error) return <p>Oops, something went wrong.</p>;
 
-  return <>{!loading && !error && <h1>Users</h1>}</>;
+  return <>{!loading && !error && <UserTable data={data} />}</>;
 };
 
 export default Users;
