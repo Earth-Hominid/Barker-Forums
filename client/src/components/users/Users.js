8b1a6dea004fe56f883c249e66ec7ab99cb768bc
C <earthhominid@gmail.com>
2022-06-21 17:38:18 -0300

    added get users ql logic to userpage

--- client/src/components/users/Users.js
+++ client/src/components/users/Users.js
@@ -1,7 +1,22 @@
 import { gql, useQuery } from '@apollo/client';
 
+const GET_USERS = gql`
+  query getUsers {
+    users {
+      id
+      username
+      email
+    }
+  }
+`;
+
 const Users = () => {
-  return <div>Users</div>;
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return <>Loading...</>;
+  if (error) return <p>Oops, something went wrong.</p>;
+
+  return <>{!loading && !error && <h1>Users</h1>}</>;
 };
 
 export default Users;
