1f581f5d2c490fdaa8a049d73a52f11383545ecc
C <earthhominid@gmail.com>
2022-06-21 23:06:54 -0300

    add user logic

--- client/src/mutations/UserMutations.js
+++ client/src/mutations/UserMutations.js
@@ -1,5 +1,15 @@
 import { gql } from '@apollo/client';
 
+const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!) {
+    addUser(username: $username, email: $email) {
+      id
+      username
+      email
+    }
+  }
+`;
+
 const DELETE_USER = gql`
   mutation deleteUser($id: ID!) {
     deleteUser(id: $id) {
@@ -10,4 +20,4 @@ const DELETE_USER = gql`
   }
 `;
 
-export { DELETE_USER };
+export { DELETE_USER, ADD_USER };
