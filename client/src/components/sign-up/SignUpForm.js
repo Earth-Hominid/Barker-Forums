46d4d5e234c12b6d7dd44b10815d0ba7a5a94236
C <earthhominid@gmail.com>
2022-06-21 23:05:13 -0300

    member signup form logic workig

--- client/src/components/sign-up/SignUpForm.js
+++ client/src/components/sign-up/SignUpForm.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link, useNavigate } from 'react-router-dom';
+import { ADD_USER } from '../../mutations/UserMutations';
+import { GET_USERS } from '../../queries/userQueries';
 
 const SignUpForm = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [addUser] = useMutation(ADD_USER, {
+    variables: { username, email },
+    update(cache, { data: { addUser } }) {
+      const { users } = cache.readQuery({ query: GET_USERS });
+
+      cache.writeQuery({
+        query: GET_USERS,
+        data: { users: [...users, addUser] },
+      });
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate('/homefeed');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (username === '' || email === '') {
+      return alert('Please complete all fields');
+    }
+
+    addUser(username, email);
+
+    setUsername('');
+    setEmail('');
+    navigate('/users');
+  };
+
   return (
     <>
       <h1 className="px-2 mt-2 text-xl font-bold text-indigo-600 text-center">
@@ -20,109 +59,20 @@ const SignUpForm = () => {
         shadow-lg
         rounded-md"
         >
-          <form action="" method="POST" className="mt-6 text-[14px] ">
+          <form className="mt-6 text-[14px]" onSubmit={handleSubmit}>
             <div className="relative">
               <input
-                id="first_name"
                 type="text"
-                name="first_name"
-                required=""
-                min="1"
-                max="50"
-                placeholder="Firstname"
-                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent
-                invalid:border-pink-500 
-                invalid:text-pink-600
-                focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-              />
-              <label
-                for="first_name"
-                className="
-                text-stone-800 
-                absolute 
-                left-0  
-                -top-3.5 
-                text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-stone-600 peer-placeholder-shown:top-2 transition-all 
-                peer-focus:-top-3.5
-                peer-foucs:text-stone-600
-                peer-focus:text-sm"
-              >
-                Firstname
-              </label>
-            </div>
-            <div className="mt-4 relative">
-              <input
-                id="last_name"
-                name="last_name"
-                required=""
-                min="1"
-                max="50"
-                type="text"
-                placeholder="Lastname"
-                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent"
-              />
-              <label
-                for="last_name"
-                className="
-              absolute
-              left-0
-              -top-3.5
-              text-sm 
-              text-stone-800
-              transition-all
-              peer-placeholder-shown:text-base
-              peer-placeholder-shown:text-stone-600
-              peer-placeholder-shown:top-2
-              peer-focus:-top-3.5
-              peer-foucs:text-stone-600
-              peer-focus:text-sm"
-              >
-                Lastname
-              </label>
-            </div>
-            <div className="mt-4 relative">
-              <input
-                id="email"
-                name="email"
-                required=""
-                type="email"
-                placeholder="Username"
-                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent
-                invalid:border-pink-500 
-                invalid:text-pink-600
-                focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-              />
-              <label
-                for="email"
-                className="
-              absolute
-              left-0
-              -top-3.5
-              text-sm 
-              text-stone-800
-              transition-all
-              peer-placeholder-shown:text-base
-              peer-placeholder-shown:text-stone-600
-              peer-placeholder-shown:top-2
-              peer-focus:-top-3.5
-              peer-foucs:text-stone-600
-              peer-focus:text-sm"
-              >
-                Email
-              </label>
-            </div>
-            <div className="mt-4 relative">
-              <input
                 id="username"
                 name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 required=""
-                type="text"
-                min="8"
                 placeholder="Username"
                 className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent"
               />
               <label
-                for="username"
+                htmlFor="username"
                 className="
               absolute
               left-0
@@ -142,17 +92,20 @@ const SignUpForm = () => {
             </div>
             <div className="mt-4 relative">
               <input
-                id="password"
-                name="password"
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required=""
-                type="password"
-                min="3"
-                max="15"
-                placeholder="Password"
-                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent"
+                placeholder="Email"
+                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent
+                invalid:border-pink-500 
+                invalid:text-pink-600
+                focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
               />
               <label
-                for="password"
+                htmlFor="email"
                 className="
               absolute
               left-0
@@ -167,11 +120,15 @@ const SignUpForm = () => {
               peer-foucs:text-stone-600
               peer-focus:text-sm"
               >
-                Password
+                Email
               </label>
             </div>
+
             <div className="mt-6 mb-12">
-              <button className="bg-indigo-600 p-2 rounded-md text-white w-full text-base hover:opacity-90">
+              <button
+                type="submit"
+                className="bg-indigo-600 p-2 rounded-md text-white w-full text-base hover:opacity-90"
+              >
                 Submit
               </button>
             </div>
