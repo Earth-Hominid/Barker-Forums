7002fc965486202e797f58c22af143ed185a9f2b
C <earthhominid@gmail.com>
2022-06-29 11:30:00 -0300

    modals

---
+++ client/components/modals/CreateSubforumModal.tsx
@@ -0,0 +1,11 @@
+import { TopSection, ModalContainer } from './Styles';
+
+const CreateSubforumModal = ({ closeModal }) => {
+  return (
+    <TopSection>
+      <ModalContainer></ModalContainer>
+    </TopSection>
+  );
+};
+
+export default CreateSubforumModal;
---
+++ client/components/modals/Styles.ts
@@ -0,0 +1,9 @@
+import tw from 'tailwind-styled-components';
+
+export const TopSection = tw.section`
+ bg-stone-300 
+ p-10`;
+
+export const ModalContainer = tw.div`
+  h-full w-full outline-none overflow-x-hidden overflow-y-auto
+ `;
