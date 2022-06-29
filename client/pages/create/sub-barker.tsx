bc8ebd82899a988c34803c1b87c60b142191416f
C <earthhominid@gmail.com>
2022-06-29 11:28:01 -0300

    create folder

---
+++ client/pages/create/sub-barker.tsx
@@ -0,0 +1,11 @@
+import AddSubForum from '../../components/add-subforum/AddSubforum';
+
+const subbarker = () => {
+  return (
+    <div className="y-7 mx-auto max-w-5xl">
+      <AddSubForum />
+    </div>
+  );
+};
+
+export default subbarker;
