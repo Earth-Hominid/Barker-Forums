5f360768e306d413bd6f88239d9fbb8ab962e2ff
C <earthhominid@gmail.com>
2022-06-30 12:48:49 -0300

    spinner fixed

---
+++ client/components/spinner/Spinner.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import { ImageContainer } from './Styles';
+
+const Spinner = require('../../public/icons/spinner.png');
+
+const LoadingSpinner = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center  inset-0 fixed bg-white">
+        <ImageContainer>
+          <Image
+            src={Spinner}
+            alt="spinner logo"
+            layout="fill"
+            objectFit="contain"
+            priority={true}
+          />
+        </ImageContainer>
+      </div>
+    </>
+  );
+};
+
+export default LoadingSpinner;
